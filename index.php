f6bf9d43f7d01693efdc698ddabc0cc52010bee3
Ishan Jain <ishan@jain.se>
2014-12-05 03:39:47 +0100

    Create a sample project, that can be used as boilerplate for bitLib. WIP.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+
+$args = array();
+$args['vendors_dir'] = realpath('Vendors');
+
+if (strpos($_SERVER['HTTP_HOST'], '.dev') !== false)
+    require_once('/home/user/projects/BitLib/src/Bit0/Core/Context.php');
+else
+    require_once('phar://' . $args['vendors_dir'] . '/BitLib.phar');
+
+class BitLibExample
+{
+    public static function Main($args = array())
+    {
+        $_App = Bit0\Core\Context::GetInstance($args['vendors_dir']);
+        $_App->InitWebContext();
+
+        $_App->Title = 'BitLibExample'; //single word
+
+        $_App->LogLevel = E_ALL;
+          $_App->SetupLogger();
+
+        $_App->AddPath(__DIR__);
+
+        //$_App->Database = new Bit0\DataSource\Adapters\MySQLi\MySQLiAdapter(
+        //    'smartorder.se', 'smartdev', 'smartorder', 'smartorder_s3', 'so_');
+
+        //$_App->User = SmartOrder\Common\User::GetBySession();
+
+        //$_App->Router->AddSecurePath('~/Inventory');
+        //$_App->Router->SetAuthPath('~/Home/Login');
+        //$_App->Router->Route('SmartOrder');
+    }
+}
+
+BitLibExample::Main($args);
